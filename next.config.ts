b0b1bb909504ec09e92dd388a40a684439e3768e
vishnu <getvishnuvs@gmail.com>
2026-02-07 14:22:09 +0530

    fix render port

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,52 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//     images: {
+//        domains: ['res.cloudinary.com'],
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'images.pexels.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.pixabay.com',
+//       },
+//        {
+//         protocol: 'https',
+//         hostname: 'i.pravatar.cc',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'live.staticflickr.com',
+//       }
+//     ],
+// }
+// }
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    images: {
-       domains: ['res.cloudinary.com'],
+  output: "standalone", // 🔥 important for render
+
+  images: {
+    domains: ["res.cloudinary.com"],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.pixabay.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: 'live.staticflickr.com',
-      }
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "cdn.pixabay.com" },
+      { protocol: "https", hostname: "i.pravatar.cc" },
+      { protocol: "https", hostname: "live.staticflickr.com" },
     ],
-}
-}
+  },
+};
 
 export default nextConfig;
